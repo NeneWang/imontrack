3ad73020c1011302b6e4df2505a720e6cf75720e
NelsonWang <wangnelson2@gmail.com>
2022-02-25 19:44:11 -0500

    screen | mains

---
+++ lib/screens/progress_feed.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CreateLog extends StatefulWidget {
+  static const routeName = '/create-log';
+  @override
+  _CreateLogState createState() => _CreateLogState();
+}
+
+class _CreateLogState extends State<CreateLog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Progress Feed")),
+    );
+  }
+}
