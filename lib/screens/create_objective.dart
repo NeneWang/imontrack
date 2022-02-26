eb26bf2230c983632df948d6dc55ecbdad67ec9c
NelsonWang <wangnelson2@gmail.com>
2022-02-26 03:27:26 -0500

    create | objective

--- lib/screens/create_objective.dart
+++ lib/screens/create_objective.dart
@@ -1,6 +1,9 @@
 import '../widgets/bottom_navigator.dart';
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import '../providers/log_provider.dart';
+
 class CreateObjective extends StatefulWidget {
   static const routeName = '/create-objective';
   @override
@@ -8,12 +11,51 @@ class CreateObjective extends StatefulWidget {
 }
 
 class _CreateObjectiveState extends State<CreateObjective> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  void _saveObjective() {
+    Provider.of<LogProvider>(context, listen: false)
+        .addObjective(_titleController.text, _descriptionController.text);
+    // Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Create Objectives")),
-      
-      bottomNavigationBar: BottomNavigator(0, context)
+      bottomNavigationBar: BottomNavigator(0, context),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: "Title"),
+                    controller: _titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Description"),
+                    controller: _descriptionController,
+                  ),
+                  RaisedButton.icon(
+                    icon: Icon(Icons.add),
+                    
+                    label: Text('Create Objective'),
+                    onPressed: _saveObjective,
+                    elevation: 0,
+                    // materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  // ElevatedButton(onPressed: onPressed, child: child)
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
