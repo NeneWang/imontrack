e852fa6962171345f904bc74847ed78844507e30
NelsonWang <wangnelson2@gmail.com>
2022-02-27 04:34:14 -0500

    decorating | menus

--- lib/screens/view_log.dart
+++ lib/screens/view_log.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -16,14 +18,14 @@ class ViewLogScreen extends StatelessWidget {
     String objectiveName = Provider.of<LogProvider>(context, listen: false)
         .getObjectiveNameByID(logEvent.objectiveID);
 
-    final List<String> tags = logEvent.tags;
-    if (logEvent.tags.length>0 || logEvent.tags != null) {
-      logEvent.tags.forEach((element) {
-        tagsWidget.add(Chip(label: Text(objectiveName)));
-    });
-    } else {
-      tagsWidget.add(Chip(label: Text("No Objective set.")));
-    }
+    // final List<String> tags = logEvent.tags;
+    // if (logEvent.tags.length>0 || logEvent.tags != null) {
+    //   logEvent.tags.forEach((element) {
+    //     tagsWidget.add(Chip(label: Text(objectiveName)));
+    // });
+    // } else {
+    //   tagsWidget.add(Chip(label: Text("No Objective set.")));
+    // }
 
     // print(logEvent);
 
@@ -32,9 +34,10 @@ class ViewLogScreen extends StatelessWidget {
         title: Text(logEvent.title),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            height: 250,
+            height: 350,
             width: double.infinity,
             child: Image.file(
               logEvent.image,
@@ -42,30 +45,26 @@ class ViewLogScreen extends StatelessWidget {
               width: double.infinity,
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(DateFormat('yyyy-MM-dd – kk:mm').format(logEvent.dateTime)),
-          Text(
-            logEvent.description,
-          ),
-          SizedBox(
-            height: 10,
-          ),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Wrap(
-                    spacing: 10,
-                    children: tagsWidget,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                      'Objective: ${objectiveName != null ? objectiveName : "No objective selected"}'),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-              ],
-            ),
-          ),
+                  Text(DateFormat('yyyy-MM-dd – kk:mm')
+                      .format(logEvent.dateTime)),
+                  Text(
+                    logEvent.description,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ))
         ],
       ),
     );
